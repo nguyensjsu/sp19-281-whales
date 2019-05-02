c67bb014f9038e7e44d9ded1fb8426f042f7e856
saliha mehboob <saliha.mehboob@sjsu.edu>
2019-05-01 18:23:16 -0700

    payment js controllers added

--- Frontend/Eclipper/app/scripts/controllers/payment.js
+++ Frontend/Eclipper/app/scripts/controllers/payment.js
@@ -1,7 +1,7 @@
 angular.module('eclipperApp')
-  .controller('paymentCtrl', function ($uibModal,paymentModel,localStorageService) {
-    var self = this,
-	init = function () {
+.controller('paymentCtrl', function ($uibModal,paymentModel,localStorageService) {
+  var self = this,
+  init = function () {
     var user = localStorageService.get("userData");
     self.payment = angular.copy(paymentModel.payment);
     if(user != null){
@@ -11,27 +11,29 @@ angular.module('eclipperApp')
   }
   self.addFunds = function() {
     item="";
-  $uibModal.open({
-     animation: true,
-     templateUrl: 'addFunds.html',
-     controller: 'addFundsCtrl',
-     controllerAs: 'afModal',
-     windowClass: 'addFunds',
-     backdrop: false,
-     resolve: {
-       item: function () {
-         return self.payment;
-       }
-     }
-   }).result.then(function(pm) {
+    $uibModal.open({
+      animation: true,
+      templateUrl: 'addFunds.html',
+      controller: 'addFundsCtrl',
+      controllerAs: 'afModal',
+      windowClass: 'addFunds',
+      backdrop: false,
+      resolve: {
+        item: function () {
+          return self.payment;
+        }
+      }
+    }).result.then(function(pm) {
       console.log(pm);
-     }, function (){
-       //TODO ERROR block
-     });
-   }
-   self.addMethods = function() {
+      self.payment = pm;
+      console.log(self.payment);
+    }, function (){
+      //TODO ERROR block
+    });
+  }
+  self.addMethods = function() {
 
-   $uibModal.open({
+    $uibModal.open({
       animation: true,
       templateUrl: 'paymentMethods.html',
       controller: 'addMethodsCtrl',
@@ -39,59 +41,75 @@ angular.module('eclipperApp')
       windowClass: 'addFunds',
       backdrop: false
     }).result.then(function(pm) {
-        console.log(self.payment);
-        self.payment.paymentMethods.push(pm);
+      console.log(self.payment);
+      self.payment.paymentMethods.push(pm);
 
-      }, function (){
-        //TODO ERROR block
-      });
-    }
+    }, function (){
+      //TODO ERROR block
+    });
+  }
 
 
 
 
   init();
-  })
-  .controller('addFundsCtrl', function ($uibModalInstance, item) {
+})
+.controller('addFundsCtrl', function ($uibModalInstance, item,paymentModel) {
 
-	  	var self = this,
-	  	init = function () {
-        self.payment = item;
-	  	}
-      self.ok = function () {
-      $uibModalInstance.close(self.payment);
-      };
-
-  self.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
-    };
-    init();
-  })
-  .controller('addMethodsCtrl', function ($uibModalInstance, paymentModel) {
-
-      var self = this,
-      init = function () {
-        self.paymentMethod = angular.copy(paymentModel.paymentMethod);
-        self.types = self.getTypes();
-        self.checkedType = "1";
-      }
-      self.ok = function () {
-        for(var i=0;i<self.types.length;i++){
-            if(self.types[i].id == self.checkedType){
-                self.paymentMethod.type = self.types[i].name;
-            }
-        }
-        $uibModalInstance.close(self.paymentMethod);
-      };
-      self.radioCheck = function() {
-        console.log(self.types);
-      }
-
-  self.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
-    };
-    self.getTypes = function () {
-      return [{"id":"1","name":"Visa"},{"id":"2","name":"Debit"}]
+  var self = this,
+  init = function () {
+    self.payment = item;
+    self.paymentMethod = null;
+    if(!(self.payment.paymentMethods.length>0)){
+      self.checkedId = "1";
+      self.types = self.getTypes();
+      self.paymentMethod = angular.copy(paymentModel.paymentMethod)
     }
-    init();
-  });
+  }
+  self.ok = function () {
+    if(self.paymentMethod != null){
+      var index = self.types.map(function(x){return x.id;}).indexOf(self.checkedId);
+      self.paymentMethod.type = self.types[index].name;
+      self.payment.paymentMethods.push(self.paymentMethod);
+    }
+    $uibModalInstance.close(self.payment);
+  };
+
+  self.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+  self.getTypes = function () {
+    return [{"id":"1","name":"Visa"},{"id":"2","name":"Debit"}]
+  }
+
+  init();
+})
+.controller('addMethodsCtrl', function ($uibModalInstance, paymentModel) {
+
+  var self = this,
+  init = function () {
+    self.paymentMethod = angular.copy(paymentModel.paymentMethod);
+    self.types = self.getTypes();
+    self.checkedType = "1";
+  }
+  self.ok = function () {
+    for(var i=0;i<self.types.length;i++){
+      if(self.types[i].id == self.checkedType){
+        self.paymentMethod.type = self.types[i].name;
+      }
+    }
+    $uibModalInstance.close(self.paymentMethod);
+  };
+  self.radioCheck = function() {
+    console.log(self.types);
+  }
+
+  self.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+  self.getTypes = function () {
+    return [{"id":"1","name":"Visa"},{"id":"2","name":"Debit"}]
+  }
+  init();
+});
