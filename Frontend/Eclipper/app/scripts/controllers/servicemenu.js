af0042b16780c167279e22c412346c8d84f734b2
Manjunatha Koni Gururaja <konman01@Manjunathas-MacBook-Pro.local>
2019-05-01 22:26:02 -0700

    chened the controller

--- Frontend/Eclipper/app/scripts/controllers/servicemenu.js
+++ Frontend/Eclipper/app/scripts/controllers/servicemenu.js
@@ -12,7 +12,7 @@ angular.module('eclipperApp')
   init();
   });*/
 
-  'use strict';
+/*  'use strict';
 
 angular.module('eclipperApp')
   .controller('ServiceMenuCtrl', function (menuService) {
@@ -29,6 +29,30 @@ angular.module('eclipperApp')
 			}, function (){
 				//TODO ERROR block
 			});
+  }
+  init();
+  });*/
+
+  
+  'use strict';
+
+angular.module('eclipperApp')
+  .controller('ServiceMenuCtrl', function (menuService) {
+    var self = this,
+        init = function () {
+          //self.menu = [];
+          //self.text ="95112";
+  }
+  self.find = function ( data) {
+    var zipcode = data;
+    console.log("submit");
+    menuService.getServiceMenu(zipcode).then(function (response){
+        self.menu = response.data;
+        //console.log(self.menu);
+      }, function (err){
+        //TODO ERROR block
+        console.log(err);
+      });
   }
   init();
   });
--- Frontend/Eclipper/app/scripts/services/menuService.js
+++ Frontend/Eclipper/app/scripts/services/menuService.js
@@ -46,7 +46,7 @@ angular.module('eclipperApp')
 	return menuFactoryService;
 });*/
 
-'use strict';
+/*'use strict';
 
 angular.module('eclipperApp')
 .factory('menuService', function ($q, $http) {
@@ -69,6 +69,46 @@ angular.module('eclipperApp')
 
 
 			  	return deferred.promise;
+
+
+
+
+        };
+
+
+        menuFactoryService.getServiceMenu = _getServiceMenu ;
+
+
+        return menuFactoryService;
+});*/
+
+'use strict';
+
+angular.module('eclipperApp')
+.factory('menuService', function ($q, $http) {
+        var menuFactoryService = {};
+        var menu = {};
+        var _getServiceMenu = function ( data ) {
+          console.log(data);
+          var urlofMenu = 'http://ec2-52-12-4-247.us-west-2.compute.amazonaws.com:8080/getMenu?zipcode='+data;
+                var request = {
+                        method: 'GET',
+                        url: urlofMenu
+                };
+// UNCOMMENT CODE TO CALL SERVICE
+
+    var deferred = $q.defer();
+
+    $http(request).then( function(response){
+            console.log(response);
+            deferred.resolve(response);
+        
+          }).catch(function(err){
+            deferred.reject(err);
+          });
+
+
+          return deferred.promise;
 
 
 
