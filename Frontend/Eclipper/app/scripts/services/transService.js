c4fae45dd8596b68ed4b3440c6d084137be521a1
prathamr <prathamesh.karve@sjsu.edu>
2019-05-03 16:54:29 -0700

    Adding controller and service for transactions front-end

---
+++ Frontend/Eclipper/app/scripts/services/transService.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('eclipperApp')
+.factory('transService', function ($q, $http) {
+	var transFactoryService = {};
+  var trans = {};
+  var _getTransHistory = function ( callback ) {
+    var request = {
+			method: 'GET',
+			url: 'http://ec2-34-216-121-186.us-west-2.compute.amazonaws.com:8000/transactions/transactions'
+			//url: configurations.transUrl + configurations.transServiceBase
+												//data: {
+												//	"UserId" : "prkarve"
+												//}
+    };
+
+		console.log("Sending GET request")
+  	var deferred = $q.defer();
+
+		$http(request).then( function(response){
+		    		deferred.resolve(response);
+			  	}).catch(function(err){
+			  		deferred.reject(err);
+			  	});
+			  	return deferred.promise;
+	};
+	
+	var _addTrans = function () {
+		var deferred = $q.defer();
+		var request = {
+			method: 'POST',
+			url: 'http://ec2-34-216-121-186.us-west-2.compute.amazonaws.com:8000/transactions/transactions',
+			data: { "ClipperId" : "123", "ServiceId" : "VTA", "Price" : 2.5, "Date" : "3 May 2019"
+			}
+		};
+
+		console.log("Sending POST request");
+		var deferred = $q.defer();
+
+		$http(request).then( function(response){
+			deferred.resolve(response);
+		}).catch(function(err){
+			deferred.reject(err);
+		});
+
+		return deferred.promise;
+	};
+
+				transFactoryService.getTransHistory = _getTransHistory;
+				transFactoryService.addTransaction  = _addTrans;
+        return transFactoryService;
+});
